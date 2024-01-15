5a529c43f07cc9f5dd5dfb857de3ae26e198fd77
Fachrur Rozi Syah Putra Berutu <rozymc99@gmail.com>
2024-01-15 17:28:33 +0700

    memperbarui web

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer class="blockquote-footer fixed-bottom">Tugas Pengembangan & Operasi <cite title="Source Title"><a href="https://shorturl.at/txCFR" target="_blank">Mata Kuliah!</a></cite></footer>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,90 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Header -->
+<?php include "header.php" ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WARKOPEMPATENAM</title>
-</head>
-<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-
-<body>
-    <div class="container col-md-6 mt-4">
-        <h1>Beri Kabar</h1>
-        <div class="card">
-            <!-- <div class="card-header bg-success text-white ">
-                DATA BARANG <a href="tambah.php" class="btn btn-sm btn-primary float-right">Tambah</a>
-            </div> -->
-            <div class="card-body">
-                <table class="table table-bordered">
-
-                    <div class="container text-center">
-                        <div class="row">
-                            <div class="col">
-                                Beranda
-                            </div>
-                            <div class="col">
-                                Kabar
-                            </div>
-                            <div class="col">
-                                <a style="color: black" href="tambah.php">Berkabar</a>
-                            </div>
-                            <div class="col">
-                                Bantuan
-                            </div>
-                        </div>
-                        <!-- <div class="row">
-                                <div class="col">Judul</div>
-                                <div class="col-4">Judul 2</div>
-                                <div class="col-4">Judul 2</div> -->
-                    </div>
-                </table>
-            </div>
-
-            <table class="table table-bordered">
-					<thead>
-						<tr>
-							<th>No</th>
-							<th>Nama</th>
-							<th>Kategori/th>
-							<th>Judul</th>
-							<th>Topik</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php
-							include('koneksi.php'); //memanggil file koneksi
-							$datas = mysqli_query($koneksi, "select * from topik") or die(mysqli_error($koneksi));
-							//script untuk menampilkan data barang
-
-							$no = 1;//untuk pengurutan nomor
-
-							//melakukan perulangan
-							while($row = mysqli_fetch_assoc($datas)) {
-						?>	
-
-					<tr>
-						<td><?= $no; ?></td>
-						<td><?= $row['nama']; //untuk menampilkan nama ?></td>
-						<td><?= $row['kategori']; ?></td>
-						<td><?= $row['judul']; ?></td>
-                        <td><?= $row['topik']; ?></td>
-						<td>
-								<a href="edit.php?id=<?= $row['id']; ?>" class="btn btn-sm btn-warning">Edit</a>
-								<a href="hapus.php?id=<?= $row['id']; ?>" class="btn btn-sm btn-danger" onclick="return confirm('anda yakin ingin hapus?');">Hapus</a>
-						</td>
-					</tr>
-
-						<?php $no++; } ?>
-					</tbody>
-				</table>
+<!-- body -->
+<div class="container mt-5">
+    <h1 class="text-center"> Selamat Datang Ke Aplikasi CRUD PHP!</h1>
+        <p class="text-center">
+            Di sini akan menggunakan PHP dan MYSQL untuk membuat CRUD(Create, Read, Update, Delete)
+        </p>
+  <div class="container">
+    <form action="includes/home.php" method="post">
+        <div class="from-group text-center">
+            <input type="submit" class="btn btn-primary mt-2" value="Let's Do it!">
         </div>
-    </div>
-    </div>
+    </form>
+  </div>
+</div>
 
-
-    <script type="text/javascript" src="assets/js/jquery-3.5.1.min.js"></script>
-    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-</body>
-
-</html>
+<!-- Footer -->
+<?php include "footer.php" ?>
